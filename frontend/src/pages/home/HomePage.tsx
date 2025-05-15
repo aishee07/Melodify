ba4bda0b53898156eddb934cf368429f02dd45b1
Aishee Mitra <130060940+aishee07@users.noreply.github.com>
2025-05-15 10:20:18 +0530

    Added dynamic greeting
    Show Good Morning/ Afternoon/ Evening/ Night according to the time of the day.

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -26,7 +26,8 @@ const HomePage = () => {
         
         if (hour < 12) return "Good Morning";
         if (hour < 18) return "Good Afternoon";
-        return "Good Evening";
+        if (hour < 20) return "Good Evening";
+        return "Good Night";
     };
     
     useEffect(() => {
